5dfe21ba8fb9160bd9c19d18ff4860dcf40a2bea
Ashley Teng <ashlaeteng@dhcp-18-111-22-203.dyn.mit.edu>
2018-07-19 16:00:37 -0400

    draft of step function

---
+++ step.py
@@ -0,0 +1,7 @@
+
+dict = {}
+def step(self):
+    random_node_index = np.round(np.random.rand()*len(self.A-1))
+    for j,node in enumerate(nodes):
+        dict[node.ID]+= self.A[i,j]
+    nodes[i].ID = max(dict,key=dict.get)
